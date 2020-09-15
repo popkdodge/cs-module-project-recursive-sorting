829ab7cc327b81e3ad48ae6528543e82be591ab3
popkdodge <popkdodge@gmail.com>
2020-09-15 18:42:58 -0400

    new

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,78 @@
 # TO-DO: Implement a recursive implementation of binary search
-def binary_search(arr, target, start, end):
+def binary_search(arr, target, left, right):
+    if left <= right:
+        midpoint = (right + left) //2
+
+        if arr[midpoint] == target:
+            return midpoint
+        elif arr[midpoint] > target:
+            return binary_search(arr, target, left, midpoint - 1)
+        else:
+            return binary_search(arr, target, midpoint + 1, right)
+    else:
+        return -1
+
+def binary_search_it(arr, target):
+    
     # Your code here
+    left = 0
+    right = len(arr) - 1
+    
+    while left <= right:
+        midpoint = (right + left) // 2
 
+        if arr[midpoint] == target:
+            return midpoint
+        
+        elif arr[midpoint] > target:
+            right = midpoint - 1
+        
+        else:
+            left = midpoint + 1
 
+    return -1  # not found
+def binary_search_it_oppo(arr, target):
+    
+    # Your code here
+    left = len(arr) - 1
+    right = 0
+    
+    while left >= right:
+        midpoint = (right + left) // 2
+
+        if arr[midpoint] == target:
+            return midpoint
+        
+        elif arr[midpoint] < target:
+            left = midpoint - 1
+        
+        else:
+            right = midpoint + 1
+
+    return -1 
 # STRETCH: implement an order-agnostic binary search
 # This version of binary search should correctly find 
 # the target regardless of whether the input array is
 # sorted in ascending order or in descending order
 # You can implement this function either recursively 
 # or iteratively
-def agnostic_binary_search(arr, target):
-    # Your code here
+def ace_dec(arr):
 
+    if len(arr) <= 1:
+        return "Array contain only one variable or it is empty!"
+    else:
+        if arr[0] > arr[1]:
+            return "DEC"
+        if arr[1] > arr[0]:
+            return "ACE"
+
+def agnostic_binary_search(arr, target):
+
+    if ace_dec(arr) != "ACE" and ace_dec(arr) != "DEC":
+        return "Array contain only one variable or it is empty!" 
+
+    if ace_dec(arr) == "ACE":
+        return binary_search_it(arr, target)
+    
+    if ace_dec(arr) == "DEC":
+        return binary_search_it_oppo(arr,target)
--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,17 +1,30 @@
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
-    merged_arr = [0] * elements
+    merged_arr = []
 
-    # Your code here
-
+    while (arrA and arrB):
+        if (arrA[0] <= arrB[0]):
+            item = arrA.pop(0)
+            merged_arr.append(item)
+        else:
+            item = arrB.pop(0)
+            merged_arr.append(item)
+    merged_arr.extend(arrA if arrA else arrB)
 
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
     # Your code here
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left =  arr[:mid]
+        right = arr[mid:]
+        left = merge_sort(left)
+        right = merge_sort(right)
 
+        while len(left)>0 and len(right)>0:
+            arr = merge(left, right)
 
     return arr
 
@@ -19,10 +32,23 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
+def merge_in_place(arr, first, mid, last):
     # Your code here
+    begin_sec_arr = mid + 1
+
+    if arr[mid] <= arr[begin_sec_arr]: #Compares the end of the first 
+        #arry to the begging of the second array
+        return None
+    
+    while first <= mid and begin_sec_arr <= last:
+        if arr[first] <= arr[begin_sec_arr]:
+            begin_sec_arr += 1
+
+        else:
+            value = arr[begin_sec_arr]
+            index = begin_sec_arr
 
 
 def merge_sort_in_place(arr, l, r):
     # Your code here
-
+    pass
